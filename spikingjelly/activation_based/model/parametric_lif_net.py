622376e3202f950aa6227b38a2bc1311237379cd
Zhou Shibo <shibo.zhou@zhejianglab.com>
2023-03-29 11:23:51 +0800

    增加DVSSignNet网络

--- spikingjelly/activation_based/model/parametric_lif_net.py
+++ spikingjelly/activation_based/model/parametric_lif_net.py
@@ -154,6 +154,41 @@ class DVSGestureNet(nn.Module):
     def forward(self, x: torch.Tensor):
         return self.conv_fc(x)
 
+class DVSSignNet(nn.Module):
+    def __init__(self, channels=128, spiking_neuron: callable = None, **kwargs):
+        super().__init__()
+
+        conv = []
+        for i in range(5):
+            if conv.__len__() == 0:
+                in_channels = 2
+            else:
+                in_channels = channels
+
+            conv.append(layer.Conv2d(in_channels, channels, kernel_size=5, padding=0, bias=False))
+            conv.append(layer.BatchNorm2d(channels))
+            conv.append(spiking_neuron(**deepcopy(kwargs)))
+            conv.append(layer.MaxPool2d(2, 2))
+
+
+        self.conv_fc = nn.Sequential(
+            *conv,
+
+            layer.Flatten(),
+            layer.Dropout(0.5),
+            layer.Linear(channels * 6 * 4, 512),
+            spiking_neuron(**deepcopy(kwargs)),
+
+            layer.Dropout(0.5),
+            layer.Linear(512, 200),
+            spiking_neuron(**deepcopy(kwargs)),
+
+            layer.VotingLayer(10)
+        )
+
+    def forward(self, x: torch.Tensor):
+        return self.conv_fc(x)
+
 def test_models():
     import torch
     from .. import neuron, surrogate, functional
